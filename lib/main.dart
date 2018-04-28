ea8e1986ce3434841a14b214a524f43da7305a56
Martín Chamarro <martin.chamarro@gmail.com>
2018-04-28 15:31:08 +0100

    EDIT -Changes to add local drawables to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'pages/splash_page.dart';
+import 'presentation/splash_page.dart';
 
 void main() => runApp(new MyApp());
 
---
+++ lib/res/drawables.dart
@@ -0,0 +1,5 @@
+
+class Drawables {
+  static const String SPLASH_BG = "assets/bg_splash.png";
+  static const String SPLASH_LOGO = "assets/splash_logo.png";
+}
