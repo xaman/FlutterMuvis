1a6e85cbc74783e7a79b2c0066f0224c43d7dae7
Martín Chamarro <martin.chamarro@gmail.com>
2018-05-03 08:31:20 +0100

    EDIT -Revert to the previous implementation of the movie item

--- lib/presentation/pages/home/movie_item.dart
+++ lib/presentation/pages/home/movie_item.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 
 import 'package:fluttermuvis/domain/model/movie.dart';
 import 'package:fluttermuvis/presentation/res/drawables.dart';
 import 'package:fluttermuvis/presentation/pages/home/movie_item_description.dart';
-
-const int _ANIMATION_DURATION = 100;
 
 class MovieItem extends StatelessWidget {
 
@@ -20,25 +17,19 @@ class MovieItem extends StatelessWidget {
       child: new InkWell(
         child: new Column(
           children: <Widget>[
-            new CachedNetworkImage(
-              imageUrl: _movie.posterPath,
-              placeholder: _getPosterPlaceHolder(),
-              errorWidget: _getPosterPlaceHolder(),
-              fadeOutDuration: new Duration(milliseconds: _ANIMATION_DURATION),
-              fadeInDuration: new Duration(milliseconds: _ANIMATION_DURATION),
-            ),
-            new MovieItemDescription(_movie),
+          new Stack(
+            children: <Widget>[
+              new Image.asset(Drawables.DEFAULT_MOVIE),
+              new Image.network(_movie.posterPath, fit: BoxFit.cover, scale: 0.5),
+            ]
+          ),
+          new MovieItemDescription(_movie),
           ],
         ),
         onTap: () => _onMovieClick(_movie),
       )
     );
   }
-
-  Widget _getPosterPlaceHolder() {
-    return new Image.asset(Drawables.DEFAULT_MOVIE, fit: BoxFit.cover);
-  }
-
 }
 
 typedef void OnMovieClickListener(Movie movie);
